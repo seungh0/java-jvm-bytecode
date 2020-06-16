9da55040439d69e09688580a2999d5a06af6a65e
seungh0 <will.seungho@gmail.com>
2020-06-16 15:10:20 +0900

    code coverage - jacoco setting

---
+++ src/main/java/me/will/Moim.java
@@ -0,0 +1,13 @@
+package me.will;
+
+public class Moim {
+
+	int maxNumberofAttendees;
+
+	int numberOfEnrollment;
+
+	public boolean isEnrollmentFull() {
+		return numberOfEnrollment >= maxNumberofAttendees;
+	}
+
+}
